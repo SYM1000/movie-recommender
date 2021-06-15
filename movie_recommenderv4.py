5b7f4f68e1be3deb6818f4456397cd0fc299f936
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-06-14 19:26:16 -0700

    Refactor

--- movie_recommenderv4.py
+++ movie_recommenderv4.py
@@ -15,13 +15,14 @@ import os
 import time
 
 def get_recommendation_server(liked_movie, sorted):
-    # start_time = time.time()
+    start_time = time.time()
     df = pd.read_csv("movie_dataset.csv")
     all_movie_titles = df["title"].to_numpy() # Array with the name of all movies from the dataframe
+    print("--- esta mierda tarda %s seconds ---" % (time.time() - start_time))
 
     #Para hacer mas rapidas las consultas: Encontrar una manera de hacer que no se calucle todo esto del cosine y se acceda como constante
     # ya que mientras tengamos el mismo dataset, siempre va a ser lo mismo
-
+    start_time = time.time()
     features = ['keywords', 'cast', 'genres', 'director']
 
     for feature in features:
@@ -42,7 +43,7 @@ def get_recommendation_server(liked_movie, sorted):
     cosine_sim_genres = cosine_similarity(count_matrix_genres)
     cosine_sim_director = cosine_similarity(count_matrix_director)
 
-    # print("--- computar el cosine tarda %s seconds ---" % (time.time() - start_time)) #This takes a lot of time
+    print("--- computar el cosine tarda %s seconds ---" % (time.time() - start_time)) #This takes a lot of time
 
     try:
         movie_index = get_index_from_title(liked_movie, df) #Gustos del usuario
@@ -233,3 +234,6 @@ def compute_recommendations_csv():
             i+=1
                 
     print("--- Recommendations computed in %s seconds ---" % (time.time() - start_time))
+
+
+get_recommendation_server("The Social Network", False)
